a4ca8c35030293492fd26f2cfedb6e11ac4e6f1d
Asher Gruber <asher@legolas-media.com>
2015-08-28 19:07:53 +0300

    First commit.

---
+++ src/main/java/ir/websearch/algo/DocumentRanker.java
@@ -0,0 +1,16 @@
+package ir.websearch.algo;
+
+public class DocumentRanker {
+
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			// must accept only one parameter which is the name of a parameter file.
+			System.out.println("Must include the parameter file name");
+			return;
+		}
+		
+		
+
+	}
+
+}
