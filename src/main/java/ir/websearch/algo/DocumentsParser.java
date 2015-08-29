610fa62fb846490f612da20d005466a04c93bd4a
Asher Gruber <asher@legolas-media.com>
2015-08-29 11:13:03 +0300

    Document parser backup.

--- src/main/java/ir/websearch/algo/DocumentsParser.java
+++ src/main/java/ir/websearch/algo/DocumentsParser.java
@@ -3,10 +3,14 @@ package ir.websearch.algo;
 import java.io.File;
 import java.io.IOException;
 import java.util.Collection;
+import java.util.regex.Matcher;
 
 import org.apache.commons.io.FileUtils;
 
 public class DocumentsParser {
+	
+	private static final String DOC_SEPERATOR = new String(new char[] { 127 });
+	private static final String DOC_PREFIX = Matcher.quoteReplacement(".I");	
 	
 	private final String docsFile;
 	
@@ -19,10 +23,11 @@ public class DocumentsParser {
 
 		File file = new File(docsFile);
 		try {
-			String docsConcat = FileUtils.readFileToString(file);
-			String [] docs = docsConcat.split(".I");
-			for (String string : docs) {
-				System.out.println("asdsd");
+			String docsJoin = FileUtils.readFileToString(file);
+			docsJoin = docsJoin.replace(DOC_PREFIX, DOC_SEPERATOR);
+			String[] docs = docsJoin.split("(?=" + DOC_SEPERATOR + ")");
+			for (String doc : docs) {
+				System.out.println(doc);
 			}
 			System.out.println("asdsd");
 		} catch (IOException e) {}
