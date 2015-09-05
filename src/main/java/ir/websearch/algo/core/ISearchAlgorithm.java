6098f276e7cf1d4108c2984bc39e7e1cffcf142c
Asher Gruber <asher@legolas-media.com>
2015-09-05 14:57:41 +0300

    Refactoring.

--- src/main/java/ir/websearch/algo/core/ISearchAlgorithm.java
+++ src/main/java/ir/websearch/algo/core/ISearchAlgorithm.java
@@ -4,6 +4,10 @@ import java.util.List;
 
 public interface ISearchAlgorithm {
 	
+	/**
+	 * The method performers search for the given document collection and query set. 
+	 * @return search results in printable formated lines (QueryID, DocID, Rank).
+	 */
 	public List<String> search();
 
 }
